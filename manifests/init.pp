d6c94c5d589556cd4d69a8177f1e8a034e7511dd
System Administrator <root@Khius.local>
2014-10-08 00:17:44 +0400

    Add service provider debian

--- manifests/init.pp
+++ manifests/init.pp
@@ -139,6 +139,7 @@ class uwsgi (
     if $service_file == undef {
         $service_file_real = $service_provider ? {
             redhat  => '/etc/init.d/uwsgi',
+            debian  => '/etc/init.d/uwsgi',
             upstart => '/etc/init/uwsgi.conf',
             default => '/etc/init/uwsgi.conf',
         }
@@ -159,6 +160,7 @@ class uwsgi (
     if $service_template == undef {
         $service_template_real = $service_provider ? {
             redhat  => 'uwsgi/uwsgi_service-redhat.erb',
+            debian  => 'uwsgi/uwsgi_service-debian.erb',
             upstart => 'uwsgi/uwsgi_upstart.conf.erb',
             default => 'uwsgi/uwsgi_upstart.conf.erb',
         }
