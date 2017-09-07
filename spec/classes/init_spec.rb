597e5fda7a2ea69769d1850688392066edb8d6f0
Oliver Bertuch <o.bertuch@fz-juelich.de>
2017-09-07 15:10:48 +0200

    RSpec testing defined type and init class with app parameter

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -16,6 +16,34 @@ describe 'uwsgi' do
         it { is_expected.to contain_class('uwsgi::packages') }
         it { is_expected.to contain_class('uwsgi::config') }
       end
+
+      context 'with app in parameters' do
+        # simple app hash
+        app_options = { 'uid' => '1000', 'gid' => '1001' }
+
+        let(:params) do
+          {
+            app: { 'test1' => app_options }
+          }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('uwsgi') }
+        it { is_expected.to contain_uwsgi__app('test1') }
+
+        case facts[:osfamily]
+        when 'Debian'
+          it {
+            is_expected.to contain_file('/etc/uwsgi/apps-enabled/test1.ini')
+              .with_ensure('present')
+          }
+        when 'RedHat'
+          it {
+            is_expected.to contain_file('/etc/uwsgi.d/test1.ini')
+              .with_ensure('present')
+          }
+        end
+      end
     end
   end
 end
