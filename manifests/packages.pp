4e0d16c05ec925262bc14f3ccf3a9b3cbc197a70
Oliver Bertuch <o.bertuch@fz-juelich.de>
2017-09-08 18:07:52 +0200

    When installing uwsgi via pip, add compiler, etc as packages to install. Add RSpec tests when the parameters install_python_dev or install_pip are set true.

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -3,7 +3,7 @@ class uwsgi::packages(
   Optional[Boolean] $install_pip,
   Optional[Boolean] $install_python_dev,
   Optional[String[1]] $python_pip,
-  Optional[String[1]] $python_dev,
+  Optional[Variant[String[1],Array[String[1],1]]] $python_dev,
 ){
   if $install_python_dev {
     ensure_packages($python_dev)
--- spec/classes/packages_spec.rb
+++ spec/classes/packages_spec.rb
@@ -16,9 +16,36 @@ describe 'uwsgi::packages' do
         when 'Debian'
           it { is_expected.to contain_package('python-dev').with_ensure('present') }
           it { is_expected.to contain_package('python-pip').with_ensure('present') }
-        # when 'RedHat'
-        #   it { is_expected.to contain_package('python-devel').with_ensure('present') }
-        #   it { is_expected.to contain_package('python2-pip').with_ensure('present') }
+        end
+      end
+
+      context 'with install_python_dev set true' do
+        let(:params) { { 'install_python_dev' => true } }
+
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('uwsgi::packages') }
+
+        case facts[:osfamily]
+        when 'Debian'
+          it { is_expected.to contain_package('python-dev').with_ensure('present') }
+          it { is_expected.to contain_package('build-essential').with_ensure('present') }
+        when 'RedHat'
+          it { is_expected.to contain_package('python-devel').with_ensure('present') }
+          it { is_expected.to contain_package('gcc').with_ensure('present') }
+        end
+      end
+
+      context 'with install_pip set true' do
+        let(:params) { { 'install_pip' => true } }
+
+        it { is_expected.to compile.with_all_deps }
+        it { is_expected.to contain_class('uwsgi::packages') }
+
+        case facts[:osfamily]
+        when 'Debian'
+          it { is_expected.to contain_package('python-pip').with_ensure('present') }
+        when 'RedHat'
+          it { is_expected.to contain_package('python2-pip').with_ensure('present') }
         end
       end
     end
